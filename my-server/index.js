685fb0b4c3ad8b19d04fb4fa92c40c67ad3d3863
Stepan <oOshusternOo@gmail.com>
2024-06-24 21:22:36 +1000

    Add routes for /static and /dynamic

--- my-server/index.js
+++ my-server/index.js
@@ -1,12 +1,36 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Привет, Октагон!</h1>');
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  res.setHeader('Content-Type', 'application/json');
+
+  if (pathname === '/static') {
+    res.statusCode = 200;
+    res.end(JSON.stringify({ header: 'Hello', body: 'Octagon NodeJS Test' }));
+  } else if (pathname === '/dynamic') {
+    const a = parseFloat(query.a);
+    const b = parseFloat(query.b);
+    const c = parseFloat(query.c);
+
+    if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+      const result = (a * b * c) / 3;
+      res.statusCode = 200;
+      res.end(JSON.stringify({ header: 'Calculated', body: result.toString() }));
+    } else {
+      res.statusCode = 400;
+      res.end(JSON.stringify({ header: 'Error' }));
+    }
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ header: 'Error' }));
+  }
 });
 
 server.listen(port, hostname, () => {
